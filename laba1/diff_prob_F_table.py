dc61d633d9cc2f5a924bcd91b3475f59532afd06
riplecs <48178483+riplecs@users.noreply.github.com>
2023-03-11 14:28:25 +0200

    Update diff_prob_F_table.py

--- laba1/diff_prob_F_table.py
+++ laba1/diff_prob_F_table.py
@@ -6,19 +6,8 @@ Created on Mon Mar  6 08:05:14 2023
 """
 
 import time
+from heys import int_to_bin, S, pi
 
-S = ('F', '8', 'E', '9', '7', '2', '0', 'D', 'C', '6', '1', '5', 'B', '4', '3', 'A')
-pi = [0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15]
-
-
-def int_to_bin(num, l):
-    num = bin(num)[2:]
-    while len(num) < l:
-        num = '0' + num
-    return num
-
-
-S = [int_to_bin(int(s, 16), 4) for s in S]
 
 V4 = [int_to_bin(i, 4) for i in range(2**4)]
 V16 = [int_to_bin(i, 16) for i in range(2**16)]
@@ -47,10 +36,11 @@ def diff_probs_table_F():
     table_S = diff_probs_table_S()
     for alpha in V16_split:
         table_alpha_i = []
-        for beta in V16_split_pi:
+        for beta in V16_split:
+            beta_ = V16_split_pi[V16_split.index(beta)]
             dp_F = 1
             for k in range(4):
-                dp_F *= table_S[alpha[k]][V4.index(beta[k])][1]
+                dp_F *= table_S[alpha[k]][V4.index(beta_[k])][1]
             if dp_F != 0:
                 table_alpha_i.append((''.join(beta), dp_F))
         file.write(str(table_alpha_i) + '\n')
