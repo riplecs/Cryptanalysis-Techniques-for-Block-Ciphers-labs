9e84368cc60c9e878e307c84635a09d6e7add7cd
riplecs <48178483+riplecs@users.noreply.github.com>
2023-05-10 10:13:27 +0300

    Update Attack.py

--- laba1/Attack.py
+++ laba1/Attack.py
@@ -12,27 +12,27 @@ def collect_ct(alpha, n = 1000):
     for x in range(n):
         with open('x.bin', 'wb') as f:
             f.write(x.to_bytes(2, 'little'))
-        subprocess.run('heys e 5 x.bin x_e.bin')
+        subprocess.run('heys e 4 x.bin x_e.bin')
         with open('x_e.bin', 'rb') as f:
             c = int.from_bytes(f.read(), 'little')
         x ^= alpha
         with open('x.bin', 'wb') as f:
             f.write(x.to_bytes(2, 'little'))
-        subprocess.run('heys e 5 x.bin x_e.bin')
+        subprocess.run('heys e 4 x.bin x_e.bin')
         with open('x_e.bin', 'rb') as f:
             c_ = int.from_bytes(f.read(), 'little')
         cipher_texts.append((c, c_))
     return cipher_texts
     
 
-def last_round_attack(cipher_texts, betas):
+def last_round_attack(cipher_texts, beta):
     Keys = []
     for k in range(1, 2**16):
         count = 0
         for (c, c_) in cipher_texts:
             y = inv_round_func(c, k)
             y_ = inv_round_func(c_, k)
-            if y_^y in betas:
+            if y_^y == beta:
                 count += 1
         Keys.append(count)
     return Keys.index(max(Keys)) + 1
@@ -40,12 +40,8 @@ def last_round_attack(cipher_texts, betas):
     
 if __name__ == '__main__':
     
-    TEXTS = collect_ct(1024)
-    beta = [273, 4369, 2184, 17]
+    TEXTS = collect_ct(24576)
+    print('Last round key: ', hex(last_round_attack(TEXTS, 17476))[2:])
     
-    print('Last round key: ', hex(last_round_attack(TEXTS, beta))[2:])
-    
-    TEXTS = collect_ct(3072)
-    beta = [273, 4368, 4369, 1, 17]
-    
-    print('Last round key: ', hex(last_round_attack(TEXTS, beta))[2:])
+    TEXTS = collect_ct(6)
+    print('Last round key: ', hex(last_round_attack(TEXTS, 8738))[2:])
